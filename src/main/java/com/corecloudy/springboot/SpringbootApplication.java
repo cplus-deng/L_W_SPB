c20b4439a62af04af380170519206830e388ac45
cplus-deng <dengydd@163.com>
2020-11-01 15:42:55 +0800

    initial commit

---
+++ src/main/java/com/corecloudy/springboot/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.corecloudy.springboot;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
